1cfe7f861f42aadc39a6f545bb1e2450f68557bc
unknown <miyukinakajima1994@gmail.com>
2019-12-24 23:02:11 +0800

    20191224230210

--- test.py
+++ test.py
@@ -1,18 +1,6 @@
 # coding=utf-8
-import tensorflow as tf
-
-flags = tf.app.flags
-FLAGS = flags.FLAGS
-tf.app.flags.DEFINE_integer("data", 10, "")
-tf.app.flags.DEFINE_boolean("istrain", "True", "")
+from tensorflow.contrib.layers.python.layers import utils
 
 
-def main(executable):
-    print(executable)
-    print(FLAGS.data)
-    print(FLAGS.istrain)
-    return
+utils.collect_named_outputs()
 
-
-if __name__ == "__main__":
-    tf.app.run()
